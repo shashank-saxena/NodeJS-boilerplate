2ea91c299e492eb9db3c15740351a98977bf4cc9
Shashank Saxena <shashank.saxena@aisera.com>
2018-04-11 12:06:58 +0530

    Added node enviroment

---
+++ validate-environment.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var semver = require('semver');
+var config = require('./package.json');
+
+var nodeVersion = config.engines.node;
+var npmVersion = config.engines.npm;
+
+if (!semver.satisfies(process.version, nodeVersion)) {
+  console.log('Required node version ' + nodeVersion + ' not satisfied with current version ' + process.version + '.');
+  process.exit(1);
+}
+
+if (!semver.satisfies(semver.clean(process.env.npm_package_engines_npm), npmVersion)) {
+  console.log('Required npm version ' + npmVersion + ' not satisfied with current version ' + process.env.npm_package_engines_npm + '.');
+  process.exit(1);
+}
